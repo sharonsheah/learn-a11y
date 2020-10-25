b80f6af36ddecca795ef01eaf30293be48c6b0eb
Sharon Sheah <sharonsheah@gmail.com>
2020-10-26 00:09:25 +1100

    add trap for tabbable elements in forms

--- js/modal.js
+++ js/modal.js
@@ -9,10 +9,37 @@ cancelButton.addEventListener('click', close);
 // Get a list of tabbable elements here:
 // https://github.com/jkup/focusable
 
+
 function open() {
+  const previouslyFocused = document.activeElement;
+
+  const focusableElements= modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled], buttom:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]');
+  modal.addEventListener('keydown', trap);
+  focusableElements = Array.prototype.slice(focusableElements);
+
+  const firstItem = focusableElements[0];
+  const lastItem = focusableElements[-1];
+
   // Show the modal and overlay
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function trap(event) {
+    if (event.keyCode === 9) {
+      // Shift is held down
+      if (event.shiftKey) {
+        // Backwards
+        if (document.activeElement === firstItem) {
+          lastItem.focus();
+        }
+      } else {
+        // Forwards
+        if (document.activeElement === lastItem) {
+          firstItem.focus();
+        }
+      }
+    }
+  }
 }
 
 function close() {
